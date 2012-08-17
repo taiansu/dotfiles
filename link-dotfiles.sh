50c229fb17cfa35d2d6af651573848ff091139fa
Taian Su <taiansu@gmail.com>
2012-08-18 01:30:08 +0800

    add autoinstall and link-dotfiles

---
+++ link-dotfiles.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+#DOTFILES=~/.dotfiles
+ DOTFILES=~/Code/dotfiles
+OHMYZSH=~/.oh-my-zsh
+
+cd
+echo "Linking Zsh files..."
+rm .zshrc
+rm .zshenv
+#ln -s $DOTFILES/zshrc .zshrc
+#ln -s $DOTFILES/zshenv .zshenv
+#source ~/.zshrc
+
+echo "Linking Git files..."
+rm .gitignore
+#rm .gitconfig
+ln -s $DOTFILES/gitignore .gitignore
+#cp $DOTFILES/gitconfig_template .gitconfig
+
+echo "Linking Rails .gemrc & .irbrc..."
+rm .gemrc
+ln -s $DOTFILES/gemrc .gemrc
+ln -s $DOTFILES/irbrc .irbrc
+
+echo "Linking oh-my-zsh custom files..."
+
+if [ -d $OHMYZSH ]; then
+  cd ~/.oh-my-zsh
+  rm -rf custom
+  ln -s ~/$DOTFILES/ohmyzsh_custom custom
+  source ~/.zshrc
+fi
+
+echo "=================================================="
+echo "All the files are linked."
+echo "Remember to edit the user info in .gitconfig, then you're ready to rock!"
+echo "=================================================="
