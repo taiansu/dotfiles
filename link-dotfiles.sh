fa1f2399ad7f5dd5c1a7afca3e9c97ba9aa1cfe9
Taian Su <taiansu@gmail.com>
2012-08-22 16:03:40 +0800

    backup origin dotfiles

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 
 #DOTFILES=~/.dotfiles
- DOTFILES=~/Code/dotfiles
+DOTFILES=~/Code/dotfiles
+BACKUPFILES=dotfiles_backup
 OHMYZSH=~/.oh-my-zsh
 
 cd
 echo "Linking Zsh files..."
-rm .zshrc
-rm .zshenv
-#ln -s $DOTFILES/zshrc .zshrc
-#ln -s $DOTFILES/zshenv .zshenv
-#source ~/.zshrc
+mv .zshrc $DOTFILES/$BACKUPFILES/zshrc.bk
+mv .zshenv $DOTFILES/$BACKUPFILES/zshenv.bk
+ln -s $DOTFILES/zshrc .zshrc
+ln -s $DOTFILES/zshenv .zshenv
+source ~/.zshrc
 
 echo "Linking Git files..."
-rm .gitignore
-#rm .gitconfig
+mv .gitignore $DOTFILES/$BACKUPFILES/gitignore.bk
+mv .gitconfig $DOTFILES/$BACKUPFILES/gitconfig.bk
 ln -s $DOTFILES/gitignore .gitignore
-#cp $DOTFILES/gitconfig_template .gitconfig
+ln -s $DOTFILES/gitconfig .gitconfig
 
 echo "Linking Rails .gemrc & .irbrc..."
-rm .gemrc
+mv .gemrc $DOTFILES/$BACKUPFILES/gemrc.bk
+mv .irbrc $DOTFILES/$BACKUPFILES/irbrc.bk
 ln -s $DOTFILES/gemrc .gemrc
 ln -s $DOTFILES/irbrc .irbrc
 
-echo "Linking oh-my-zsh custom files..."
+echo "Copying oh-my-zsh custom files..."
 
 if [ -d $OHMYZSH ]; then
   cd ~/.oh-my-zsh/custom
