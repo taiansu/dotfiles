be1bb94f77b4362b02bb45a8eb651af83b53ed14
Taian Su <me@taian.su>
2014-08-07 12:24:22 +0800

    remove npm_updater

--- npm_updater.sh
+++
@@ -1,7 +0,0 @@
-for man in 1 3 5 7; do
-  ln -sf /usr/local/lib/node_modules/npm/man/man${man}/* /usr/local/share/man/man${man}
-done
-
-ln -sf /usr/local/lib/node_modules/npm/bin/npm-cli.js /usr/local/bin/npm
-
-npm update npm -g
