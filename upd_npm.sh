a917aa0a6cd72f9ed9bd887d7bd8759459d1b7a0
Taian Su <me@taian.su>
2014-12-25 17:57:19 +0800

    fix npm updater

--- upd_npm.sh
+++ upd_npm.sh
@@ -5,5 +5,8 @@ set -x
 
 for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2)
 do
+  # echo "$package"
+  if [[ ! "$package" =~ ^npm.*$ ]]; then
     npm -g install "$package"
+  fi
 done
