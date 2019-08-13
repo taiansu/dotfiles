3f4142083cb0f2b40478920961c7f6228ed0c9f5
Tai An Su <taiansu@gmail.com>
2019-08-14 02:31:20 +0800

    Improvements

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 ### fisher
 if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    set -q XDG_CONFIG_HOME
+    or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
@@ -36,56 +37,84 @@ set -U FZF_DEFAULT_COMMAND 'ag -l'
 set -U GOPATH $HOME/projects/gocode/bin
 set -U GO_CUSTOM_PATH /usr/local/opt/go/libexec/bin
 
-[ -d $GOPATH ]; and set PATH $PATH $GOPATH
-[ -d $GO_CUSTOM_PATH ]; and set PATH $PATH $GO_CUSTOM_PATH
+[ -d $GOPATH ]
+and set PATH $PATH $GOPATH
+[ -d $GO_CUSTOM_PATH ]
+and set PATH $PATH $GO_CUSTOM_PATH
 
 # Yarn
 # set -U YARN_PATH $HOME/.config/yarn/global/node_modules/.bin
 # [ -d $YARN_PATH ]; and set PATH $PATH $YARN_PATH
 
+### functions
+function expand-dot-to-parent-directory-path -d 'expand ... to ../.. etc'
+    # Get commandline up to cursor
+    set -l cmd (commandline --cut-at-cursor)
+
+    # Match last line
+    switch $cmd[-1]
+        case '*..'
+            commandline --insert '/.'
+        case '*'
+            commandline --insert '.'
+    end
+end
+
+bind . 'expand-dot-to-parent-directory-path'
+
 ### aliases
-alias n "tab"
+abbr o "open"
 
-alias v "nvim"
+abbr n "tab"
 
-alias ll "ls -al"
+abbr v "nvim"
 
-alias g "hub"
+abbr ll "ls -al"
 
-alias tl "tail"
+abbr g "hub"
+
+abbr tl "tail"
 
 # Headless Browsers
-alias chrome "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-alias firefox "/Applications/Firefox.app/Contents/MacOS/firefox"
+abbr chrome "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+abbr firefox "/Applications/Firefox.app/Contents/MacOS/firefox"
 
 # Youtube mp3
-alias youtube-mp3 "youtube-dl --extract-audio --audio-format mp3"
+abbr youtube-mp3 "youtube-dl --extract-audio --audio-format mp3"
 
 # less
-alias lf "less +F"
+abbr lf "less +F"
 
 # Make unified diff syntax the default
-alias diff "diff -u"
+abbr diff "diff -u"
 
 # octal+text permissions for files
-alias perms "stat -c '%A %a %n'"
+abbr perms "stat -c '%A %a %n'"
 
 #clean open_with
-alias cleanOpenWith "/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user"
+abbr cleanOpenWith "/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user"
 
 # bundle & rake
-alias k "rake"
-alias b "bundle"
-alias br "bundle exec rake"
+abbr k "rake"
+abbr b "bundle"
+abbr br "bundle exec rake"
 
 # mix
-alias m "mix"
-alias im "iex -S mix"
+abbr m "mix"
+abbr im "iex -S mix"
+abbr imt "env MIX_ENV=test iex -S mix"
+abbr imp "env MIX_ENV=prod iex -S mix"
 
 # vagrant
-alias vg "vagrant"
+abbr vg "vagrant"
+
+# please
+alias please "sudo"
 
 # [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
-[ -f ~/.iterm2_shell_integration.fish ]; and source ~/.iterm2_shell_integration.fish
-[ -f ~/.asdf/asdf.fish ]; and source ~/.asdf/asdf.fish
-[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+[ -f ~/.iterm2_shell_integration.fish ]
+and source ~/.iterm2_shell_integration.fish
+[ -f ~/.asdf/asdf.fish ]
+and source ~/.asdf/asdf.fish
+[ -f /usr/local/share/autojump/autojump.fish ]
+and source /usr/local/share/autojump/autojump.fish
